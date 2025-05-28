ea6883b5de140500f9101314072f9d045087afaf
Adam Poper <adampoper@Adams-MacBook-Pro.local>
2025-05-27 22:03:42 -0400

    final touches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dot11services';
+export class AppComponent implements AfterViewInit {
+	ngAfterViewInit() {
+		const navbar = document.querySelector('.navbar') as HTMLElement;
+		if (navbar) {
+			const navbarHeight = navbar.offsetHeight;
+			document.documentElement.style.setProperty('--navbar-height', `${navbarHeight}px`);
+		}
+	}
 }
