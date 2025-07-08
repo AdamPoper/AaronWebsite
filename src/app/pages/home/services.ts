ac5722eb7cb18d053554b44d2da1fe62e1dd4f61
Adam Poper <adampoper@Adams-MacBook-Pro.local>
2025-07-07 21:55:39 -0400

    changing technical services and tweaks to about content

--- src/app/pages/home/services.ts
+++ src/app/pages/home/services.ts
@@ -6,19 +6,19 @@ export type ServiceItem = {
 
 export const services = [
     {
+        title: 'Post Deployment Network Support',
+        description: 'Even after a deployment, we are not hands-off until all devices are functional on the network.'
+    },
+    {
         title: 'Wireless Design & Implementation',
         description: 'Wi-Fi networks built from the ground up with the purpose of overhauling existing Wi-Fi networks or adding Wi-Fi for the first time.'
     },
     {
-        title: 'Device Installation & Support',
-        description: 'Adding a fleet of new office or end user equipment to an existing Wi-Fi network.'
+        title: 'In-Wall Hospitality Deployments',
+        description: 'Wireless Access Points with built in LAN ports to service small deployments or office rooms. These In-Wall units sit low with a small profile.'
     },
     {
-        title: 'Wi-Fi 7 Migrations',
-        description: 'Designing, implementing, and/or upgrading a wireless network around the 6Ghz spectrum with Wi-Fi 7 technologies.'
-    },
-    {
-        title: 'Network Support',
-        description: 'Resolutions for when devices will not communicate with each other, the network, or the internet.'
+        title: 'Managed Network',
+        description: 'Ongoing network support at an affordable price.'
     }
 ] as ServiceItem[];
