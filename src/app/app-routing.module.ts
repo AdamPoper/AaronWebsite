9c7896e374d28f830040d429334b8647db59b4b4
Adam Poper <adampoper@Adams-MacBook-Pro.local>
2025-08-05 21:26:06 -0400

    adding our method page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { MethodComponent } from './pages/method/method.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
+    { path: 'our-method', component: MethodComponent },
     { path: '**', redirectTo: '' } // Redirect unknown routes to Home
 ];
 
